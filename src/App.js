a1ea42f7395f55d9ea4480dbafdc021983cd2e23
Enrique Bermúdez <enrique.bermudez@4rsoluciones.com>
2020-02-17 15:06:28 -0300

    feat: add styles files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 // @flow
 
 import React from 'react';
+import main from './assets/styles/main.scss';
 
-const App = () => <>React!</>;
+const App = () => (
+  <>
+    <div className="container">
+      <div className="title">React Boilerplate</div>
+    </div>
+  </>
+);
 
 export default App;
